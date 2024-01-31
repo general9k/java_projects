0ebcf58e9efe25a75b598e077b4bcd8e10444a81
general_9k <rodion.maksik2003@mail.ru>
2024-01-31 14:09:14 +0300

    new_file

---
+++ Library2_JPA/src/main/java/ru/rodionov/spring_library/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package ru.rodionov.spring_library.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.rodionov.spring_library.models.Person;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByFullName(String fullName);
+}
